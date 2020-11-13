4fb97535ebb4751f5341fadf03745034dd263ae4
aljt <alvinljt@gmail.com>
2020-11-14 00:43:35 +0800

    modified main page ejs and data.json

--- index.js
+++ index.js
@@ -24,12 +24,39 @@ const whenRequestForMainPage = (request, response) => {
     }
 
     // if no reading error ----------------------
+    // get the navbar object from data.json JS object
+    const { navbar } = dataJsObject;
+
     // get the mainpage object from the data.json JS object
     const { mainPage } = dataJsObject;
-    console.log(dataJsObject);
+
+    // set the ejs data
+    const ejsData = {
+      navbar,
+      mainPage,
+    };
 
     // render the main page object to ejs
-    response.render('main-page', mainPage);
+    response.render('main-page', ejsData);
+  });
+};
+
+// request callback function to render data of the category of ingredient requested to ejs
+const whenRequestForCategory = (request, respond) => {
+  console.log('request came in');
+
+  // read the JSON file and convert contents to a JS object and pass to callback
+  read('data.json', (dataJsObject, error) => {
+    // return reading error if there is
+    if (error) {
+      console.log('error reading file');
+      response.send(`Error reading file: ${error}`);
+      return;
+    }
+
+    // if no reading error ------------------------------------
+    // get the ingredient category requested
+    const { ingredient } = request.params;
   });
 };
 
@@ -75,6 +102,9 @@ const whenRequestForRecipe = (request, response) => {
 
 // set the route for main page
 app.get('/', whenRequestForMainPage);
+
+// set the route for ingredient category lists
+app.get('/category/:ingredient', whenRequestForCategory);
 
 // set the route for recipe
 app.get('/recipe/:index', whenRequestForRecipe);
